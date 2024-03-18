340b03b688d6b1d2f73778e6b25efda364dd1d51
Erblin <erbliny22@gmail.com>
2024-03-18 23:07:12 +0100

    React and Express Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
