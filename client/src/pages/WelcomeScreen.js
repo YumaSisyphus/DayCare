30079585a92e6ffa42c6a2dddb38b68f7a990cf3
Erblin <erbliny22@gmail.com>
2024-03-22 22:03:01 +0100

    Fonts and Colors

--- client/src/pages/WelcomeScreen.js
+++ client/src/pages/WelcomeScreen.js
@@ -1,13 +1,48 @@
-import { Button, TextField, Box } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import { Box, Container, Typography } from "@mui/material";
+import { Colors } from "../utils/colors";
+import { motion } from "framer-motion";
+import Logo from "../images/logo.jpg";
 
 const WelcomeScreen = () => {
   return (
     <div>
-      <Typography>Hello</Typography>
-      <Button>Buton</Button>
-      <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
-        <TextField label="Test" />
+      <Box width={"100%"} sx={{ bgcolor: `${Colors.primary}` }}>
+        <Container maxWidth="xl">
+          <Box
+            height={"400px"}
+            display={"flex"}
+            position={"relative"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={20}
+            p={5}
+          >
+            <img src={Logo} alt="logo" className="welcome-screen-logo" />
+            <Box zIndex={1} width={"30%"}>
+              <Typography variant="h2" fontFamily={"'Baloo 2', sans-serif"}>
+                LET Center
+              </Typography>
+              <Typography variant="h6" fontFamily={"'Baloo 2', sans-serif"}>
+                "Quality Care for Your Little Ones" "Safe, Engaging, and
+                Nurturing Environment" "Discover Our Programs and Enroll Today"
+              </Typography>
+            </Box>
+            <motion.div
+              className="welcome-screen-top-animation"
+              animate={{
+                scale: [1, 2, 2, 1, 1],
+                rotate: [0, 0, 180, 180, 0],
+                borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+              }}
+              transition={{
+                duration: 2,
+                ease: "easeInOut",
+                times: [0, 0.2, 0.5, 0.8, 1],
+                repeat: Infinity,
+              }}
+            />
+          </Box>
+        </Container>
       </Box>
     </div>
   );
---
+++ client/src/utils/colors.js
@@ -0,0 +1,5 @@
+export const Colors = {
+  primary: "#fff3e5",
+  secondary: "#fed5c6",
+  lightbg: "#fff0ec",
+};
