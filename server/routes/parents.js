0079c4327a94aa30a8512aec6b8eb143fc562f80
Erblin <erbliny22@gmail.com>
2024-04-04 20:16:20 +0200

    Edit Parents fix

--- server/routes/parents.js
+++ server/routes/parents.js
@@ -29,7 +29,7 @@ router.get("/getParents", (req, res) => {
   });
 });
 
-router.get("/getParent", (req, res) => {
+router.get("/getParent/:parentId", (req, res) => {
   const sql = "SELECT * FROM parent WHERE ParentId=?";
   const values = [req.params.parentId];
   db.query(sql, [values], (err, data) => {
@@ -62,7 +62,6 @@ router.delete("/deleteParent/:parentId", (req, res) => {
   });
 });
 
-
 router.post("/createParent", (req, res) => {
   const sql =
     "INSERT INTO parent (Name, Surname,Birthday, Gender,Email, Address, PhoneNumber, Username, Password, Active) VALUES (?)";
@@ -86,39 +85,39 @@ router.post("/createParent", (req, res) => {
       return res.json({
         success: true,
         message: "Create parent succesful",
-        test:data[0]
+        test: data[0],
       });
     }
   });
 });
 
 router.put("/updateParent", (req, res) => {
-    const sql =
-      "UPDATE parent SET  Name=?, Surname=?, Birthday=?, Gender=?, Email=?, Address=?, PhoneNumber=?, Username=?, Password=?, Active=? WHERE ParentId=?";
-    const values = [
-      req.body.name,
-      req.body.surname,
-      req.body.birthday,
-      req.body.gender,
-      req.body.email,
-      req.body.address,
-      req.body.phonenumber,
-      req.body.username,
-      req.body.password,
-      req.body.active,
-      req.body.parentId
-    ];
-    db.query(sql, values, (err, data) => {
-      if (err) {
-        console.error("Database error:", err);
-        return res.json({ success: false, message: "Update parent failed" });
-      } else {
-        return res.json({
-          success: true,
-          message: "Update parent successful",
-        });
-      }
-    });
+  const sql =
+    "UPDATE parent SET  Name=?, Surname=?, Birthday=?, Gender=?, Email=?, Address=?, PhoneNumber=?, Username=?, Password=?, Active=? WHERE ParentId=?";
+  const values = [
+    req.body.name,
+    req.body.surname,
+    req.body.birthday,
+    req.body.gender,
+    req.body.email,
+    req.body.address,
+    req.body.phonenumber,
+    req.body.username,
+    req.body.password,
+    req.body.active,
+    req.body.parentId,
+  ];
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      console.error("Database error:", err);
+      return res.json({ success: false, message: "Update parent failed" });
+    } else {
+      return res.json({
+        success: true,
+        message: "Update parent successful",
+      });
+    }
+  });
 });
 
 module.exports = router;
