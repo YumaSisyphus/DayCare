0243522c8a5d15c119f9da99d0548e0ef2de8001
butrinnt_ <butritn.reqica@gmail.com>
2024-05-28 00:53:49 +0200

    Fixed issue with server.js
    I added the mongoDb part to the server.js. It's using my hosted Database for now, install your own on your local machine and connect it for better results.

--- server/server.js
+++ server/server.js
@@ -1,47 +1,49 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // Initialize Stripe with your API key
 
-  app.use(cors());
-  app.use(express.json()); // Add this line to parse JSON bodies
-  app.use(express.urlencoded({ extended: true })); // Add this line to parse URL-encoded bodies
+app.use(cors());
+app.use(express.json()); // Add this line to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Add this line to parse URL-encoded bodies
 
-  mongoose.connect("mongodb://localhost:27017/daycare", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
-  
-  const loginRoutes = require("./routes/login");
-  const childrenRoutes = require("./routes/children");
-  const parentsRoutes = require("./routes/parents");
-  const activityRoutes = require("./routes/activity");
-  const foodRoutes = require("./routes/food");
-  const classRoutes = require("./routes/class");
-  const ageGroupRoutes = require("./routes/agegroup");
-  const mealRoutes = require("./routes/meal");
-  const staffRoutes = require("./routes/staff");
-  const contactformRoutes = require("./routes/contactform");
-  const paymentRoutes= require("./routes/payment");
-  
-  app.use("/login", loginRoutes);
-  app.use("/children", childrenRoutes);
-  app.use("/parents", parentsRoutes);
-  app.use("/activity", activityRoutes);
-  app.use("/food", foodRoutes);
-  app.use("/class", classRoutes);
-  app.use("/agegroup", ageGroupRoutes);
-  app.use("/meal", mealRoutes);
-  app.use("/staff", staffRoutes);
-  app.use("/contactform", contactformRoutes);
-  app.use("/payment",paymentRoutes);
+mongoose
+  .connect(
+    "mongodb+srv://butritnreqica:EwKrYj7B9y586W1G@cluster0.ufzgp4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //"mongodb://localhost:27017/daycare"
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-  
+const loginRoutes = require("./routes/login");
+const childrenRoutes = require("./routes/children");
+const parentsRoutes = require("./routes/parents");
+const activityRoutes = require("./routes/activity");
+const foodRoutes = require("./routes/food");
+const classRoutes = require("./routes/class");
+const ageGroupRoutes = require("./routes/agegroup");
+const mealRoutes = require("./routes/meal");
+const staffRoutes = require("./routes/staff");
+const contactformRoutes = require("./routes/contactform");
+const paymentRoutes = require("./routes/payment");
 
-  app.listen(5000, () => {
-    console.log("Server started on port 5000");
-  });
+app.use("/login", loginRoutes);
+app.use("/children", childrenRoutes);
+app.use("/parents", parentsRoutes);
+app.use("/activity", activityRoutes);
+app.use("/food", foodRoutes);
+app.use("/class", classRoutes);
+app.use("/agegroup", ageGroupRoutes);
+app.use("/meal", mealRoutes);
+app.use("/staff", staffRoutes);
+app.use("/contactform", contactformRoutes);
+app.use("/payment", paymentRoutes);
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
