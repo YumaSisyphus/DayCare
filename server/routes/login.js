50c108a1ca5425dc44b657933178eba4e5b93524
butrinnt_ <butritn.reqica@gmail.com>
2024-05-29 01:33:44 +0200

    Implemented password encryption, now you can log in with email or username. Previous created users will not work, check description
    on the servers side:
    npm i bcrypt
    use this to create user with hashed password on the database:
    `insert into daycare.staff (Name, Surname, Birthday, Gender, Email, PhoneNumber, Role, Username, Address, Password)
     VALUES('t','t','2015-02-20','Fe','t@gmail','678686','Admin','tUsername','address','$2b$10$.2zl2lTl6yS1oMH9q0Whk.qsTwhezW1HAA/NTLgNRTqpFgo4ErSzS')`
    '$2b$10$.2zl2lTl6yS1oMH9q0Whk.qsTwhezW1HAA/NTLgNRTqpFgo4ErSzS' is the letter 'a' so when you log in type 'a' as password.

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,6 +4,7 @@ const crypto = require("crypto");
 const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 
 router.use(express.json());
 router.use(cookieParser());
@@ -22,78 +23,23 @@ router.post("/", (req, res) => {
   const { username, password } = req.body;
 
   // Check in staff table
-  const staffSql = "SELECT * FROM staff WHERE Username = ? AND Password = ?";
-  const staffValues = [username, password];
+  const staffSql = "SELECT * FROM staff WHERE Username = ? Or Email = ?";
+  const staffValues = [username, username];
 
   db.query(staffSql, staffValues, (staffErr, staffData) => {
     if (staffErr) {
       console.error("Staff database error:", staffErr);
       return res.json({ success: false, message: "Login Failed" });
     }
-
     if (staffData && staffData.length > 0) {
-      const accessToken = jwt.sign(
-        {
-          userId: staffData[0].StaffId,
-          username: staffData[0].Username,
-          userType: "staff",
-          role: staffData[0].Role,
-        },
-        secretKey,
-        { expiresIn: "1h" }
-      );
-
-      const refreshToken = jwt.sign(
-        {
-          userId: staffData[0].StaffId,
-          username: staffData[0].Username,
-          userType: "staff",
-          role: staffData[0].Role,
-        },
-        refreshTokenSecretKey,
-        { expiresIn: "7d" }
-      );
-
-      res.cookie("token", accessToken, {
-        httpOnly: true,
-        maxAge: 3600000, // 1 hour
-      });
-
-      res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        maxAge: 604800000, // 7 days
-      });
-
-      return res.json({
-        success: true,
-        message: "Login Successful",
-        token: accessToken,
-        user: {
-          id: staffData[0].StaffId,
-          username: staffData[0].Username,
-          role: staffData[0].Role, // This line is important
-          userType: "staff",
-        },
-      });
-    } else {
-      // Check in parent table
-      const parentSql =
-        "SELECT * FROM parent WHERE Username = ? AND Password = ?";
-      const parentValues = [username, password];
-
-      db.query(parentSql, parentValues, (parentErr, parentData) => {
-        if (parentErr) {
-          console.error("Parent database error:", parentErr);
-          return res.json({ success: false, message: "Login Failed" });
-        }
-
-        if (parentData && parentData.length > 0) {
+      bcrypt.compare(password, staffData[0].Password, (err, isMatch) => {
+        if (isMatch) {
           const accessToken = jwt.sign(
             {
-              userId: parentData[0].ParentId,
-              username: parentData[0].Username,
-              role: "parent", // This line is important
-              userType: "parent",
+              userId: staffData[0].StaffId,
+              username: staffData[0].Username,
+              userType: "staff",
+              role: staffData[0].Role,
             },
             secretKey,
             { expiresIn: "1h" }
@@ -101,10 +47,10 @@ router.post("/", (req, res) => {
 
           const refreshToken = jwt.sign(
             {
-              userId: parentData[0].ParentId,
-              username: parentData[0].Username,
-              role: "parent", // This line is important
-              userType: "parent",
+              userId: staffData[0].StaffId,
+              username: staffData[0].Username,
+              userType: "staff",
+              role: staffData[0].Role,
             },
             refreshTokenSecretKey,
             { expiresIn: "7d" }
@@ -112,7 +58,7 @@ router.post("/", (req, res) => {
 
           res.cookie("token", accessToken, {
             httpOnly: true,
-            maxAge: 3600000, // 1 hour  3600000
+            maxAge: 3600000, // 1 hour
           });
 
           res.cookie("refreshToken", refreshToken, {
@@ -125,11 +71,86 @@ router.post("/", (req, res) => {
             message: "Login Successful",
             token: accessToken,
             user: {
-              id: parentData[0].ParentId,
-              username: parentData[0].Username,
-              role: "parent", // This line is important
-              userType: "parent",
+              id: staffData[0].StaffId,
+              username: staffData[0].Username,
+              role: staffData[0].Role, // This line is important
+              userType: "staff",
             },
+          });
+        } else if (err) {
+          console.error("Error during log in:" + err);
+        } else {
+          return res.json({
+            success: false,
+            message: "Invalid username or password",
+          });
+        }
+      });
+    } else {
+      // Check in parent table
+      const parentSql = "SELECT * FROM parent WHERE Username = ? Or Email = ?";
+      const parentValues = [username, username];
+
+      db.query(parentSql, parentValues, (parentErr, parentData) => {
+        if (parentErr) {
+          console.error("Parent database error:", parentErr);
+          return res.json({ success: false, message: "Login Failed" });
+        }
+
+        if (parentData && parentData.length > 0) {
+          bcrypt.compare(password, parentData[0].Password, (err, isMatch) => {
+            if (isMatch) {
+              const accessToken = jwt.sign(
+                {
+                  userId: parentData[0].ParentId,
+                  username: parentData[0].Username,
+                  role: "parent", // This line is important
+                  userType: "parent",
+                },
+                secretKey,
+                { expiresIn: "1h" }
+              );
+
+              const refreshToken = jwt.sign(
+                {
+                  userId: parentData[0].ParentId,
+                  username: parentData[0].Username,
+                  role: "parent", // This line is important
+                  userType: "parent",
+                },
+                refreshTokenSecretKey,
+                { expiresIn: "7d" }
+              );
+
+              res.cookie("token", accessToken, {
+                httpOnly: true,
+                maxAge: 3600000, // 1 hour  3600000
+              });
+
+              res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                maxAge: 604800000, // 7 days
+              });
+
+              return res.json({
+                success: true,
+                message: "Login Successful",
+                token: accessToken,
+                user: {
+                  id: parentData[0].ParentId,
+                  username: parentData[0].Username,
+                  role: "parent", // This line is important
+                  userType: "parent",
+                },
+              });
+            } else if (err) {
+              console.error("Error during log in:" + err);
+            } else {
+              return res.json({
+                success: false,
+                message: "Invalid username or password",
+              });
+            }
           });
         } else {
           return res.json({
@@ -173,7 +194,6 @@ router.post("/token/refresh", (req, res) => {
   const refreshToken = req.cookies.refreshToken;
   if (refreshToken) {
     try {
-      console.log(refreshTokenSecretKey);
       const decoded = jwt.verify(refreshToken, refreshTokenSecretKey);
       const newAccessToken = jwt.sign(
         {
